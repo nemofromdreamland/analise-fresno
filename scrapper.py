64a46ba1ebb4ae89284f2e3cb5dcd59df3c2d457
nemofromdreamland <rafafrassetto@outlook.com>
2025-06-10 12:04:38 -0300

    raspagem de dados e criação do primeiro codigo para organizar em csv

--- scrapper.py
+++ scrapper.py
@@ -10,6 +10,9 @@ letra: Desastre, tragédia, beirando a comédia
 Eu nunca fui ...,2024,Quando o Pesadelo Acabar,Desastre...
 """
 
+from csv import DictWriter
+
+import dateparser
 from httpx import get
 from parsel import Selector
 
@@ -30,3 +33,39 @@ def faixas(url: str) -> list[tuple[str, str]]:
         (musica.css('h3::text').get().strip(), musica.css('a').attrib['href'])
         for musica in musicas
     ]
+
+def discos(url: str) -> list[tuple[str | None, ...]]:
+    response = get(url)
+    s = Selector(response.text)
+    discos = s.css('.ddfKcW')
+
+    resultado = []
+
+    for disco in discos:
+        disco_url = disco.css('.jsXEqK').attrib['href']
+        disco_nome = disco.css('.gSnatN::text').get()
+        disco_ano = disco.css('.ixmAQP::text').get()
+        
+        resultado.append(
+            (disco_url, disco_nome, disco_ano)
+        )
+
+    return resultado
+
+url = 'https://genius.com/artists/Fresno/albums'
+
+with open('deadfish.csv', 'w') as f:
+    writer = DictWriter(f, ['album', 'data', 'musica', 'letra'])
+    writer.writeheader()
+    for disco in discos(url):
+        for faixa in faixas(disco[0]):
+            row = {
+                'album': disco[1],
+                'data': dateparser.parse(disco[2]),
+                'musica': faixa[0],
+                'letra': letra(faixa[1])
+            }
+            print(row)
+            writer.writerow(row)
+        
+    
